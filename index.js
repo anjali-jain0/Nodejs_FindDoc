08f6c4d7e44c20787765637d6bbe0a62521a481e
anjali-jain0 <jainanjali043@gmail.com>
2019-12-07 10:38:17 +0530

    update readme and screenshots

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 var express=require('express');
+var nodemailer = require('nodemailer');
+var Nexmo=require('nexmo');
 
 var app=express();
 
 var session = require('express-session');
 var flash=require('connect-flash');
 var expressValidator=require('express-validator');
+
 
 app.set("view engine",'ejs'); 
 
@@ -14,6 +17,11 @@ var bodyParser=require('body-parser');
 
 var urlencodedParser=bodyParser.urlencoded({extended: false});
 
+const nexmo = new Nexmo({
+  apiKey: '0aabd16f',
+  apiSecret: 'sxBlxAb8zkpkxPSU'
+},{debug:true});
+
 app.use(session({
   secret: 'keyboard cat',
   resave: true,
@@ -21,6 +29,7 @@ app.use(session({
 }));
 
 app.use(require('connect-flash')());
+
 app.use(function (req, res, next) {
   res.locals.messages = require('express-messages')(req, res);
   next();
@@ -47,7 +56,10 @@ app.use(expressValidator({
 
 var mongoose=require('mongoose');
 
-mongoose.connect('mongodb://project:project5@ds229722.mlab.com:29722/project5find');
+mongoose.connect('mongodb://localhost/find_doc');
+
+//??
+//var db = mongoose.connection;
 
 var DoctorSchema = new mongoose.Schema({
 	name:String,
@@ -60,36 +72,58 @@ var DoctorSchema = new mongoose.Schema({
 	state:String,
 	zipcode:Number,
 	phnnumber:String,
+	fee:String,
+	votes:Number,
+	gender:String,
+	exp : Array,
+	//msgs : Array
+});
+
+var RecordSchema = new mongoose.Schema({
+	title:String,
+	name:String,
+	date:String,
+	type:String
+});
+
+var PatientSchema = new mongoose.Schema({
+	pname:String,
+	pnumber:String,
+	page:String,
+	pemail:String,
+	pgender:String,
+	pdate:String,
+	ptime:String,
+	paddress:String,
+});
+
+var TestSchema = new mongoose.Schema({
+	test:String,
+	labname:String,
+	address:String,
+	facilties:String,
+	amount:String,
 });
 
 var AppointmentSchema = new mongoose.Schema({
 	name:String,
 	date:String,
-	age:Number
+	age:String,
+	email:String,
+	docname:String,
+	mobile:String
 });
 
 var CategorySchema = new mongoose.Schema({
 	category:String
 }); 
 
-var Doctor= mongoose.model('Doctor',DoctorSchema);
-var Category= mongoose.model('Category',CategorySchema);
-var Appointment= mongoose.model('Appointment',AppointmentSchema);
-
-var categoryOne= Category({category:'General'}).save(function(err){
-	if(err) throw err;
-});
-var categoryTwo= Category({category:'Neurologist'}).save(function(err){
-	if(err) throw err;
-});
-
-var doctorOne= Doctor({phnnumber:'0000',name:'Rayansh Singh',category:'General',accepting:'true',pname:'Dr. Ray',address:'x1',year:'1998',city:'Udaipur',state:'Rajasthan',zipcode:'19900'}).save(function(err){
-	if(err) throw err;
-});
-
-var doctorTwo= Doctor({phnnumber:'1111',name:'Aahana Sharma',category:'Neurologist',accepting:'true',pname:'Dr. Hana',address:'x2',year:'2000',city:'Gwalior',state:'Madhya Pradesh',zipcode:'12000'}).save(function(err){
-	if(err) throw err;
-});
+var Patient= mongoose.model('Patient',PatientSchema,'Patient');
+var Test= mongoose.model('Test',TestSchema,'Test');
+var Record= mongoose.model('Record',RecordSchema,'Record');
+var Doctor= mongoose.model('Doctor',DoctorSchema,'Doctor');
+var Category= mongoose.model('Category',CategorySchema,'Category');
+var Appointment= mongoose.model('Appointment',AppointmentSchema,'Appointment');
 
 app.get('/doctors/:category',function(req,res){
 	var query={category:req.params.category};
@@ -97,7 +131,129 @@ app.get('/doctors/:category',function(req,res){
 		if(err) throw err;
 		res.render('project5browse',{doctors:data});
 	});
+});
 
+app.get('/records',function(req,res){
+
+	res.render('project5records1');
+});
+
+app.post('/record2',urlencodedParser,function(req,res){
+
+	const mobileno=req.body.mobileno;
+	req.checkBody('mobileno','Mobile Number should be valid').isMobilePhone();
+	app.locals.fullname=req.body.fullname;
+	req.checkBody('fullname','Name should be atleast 3 characters long').isLength({min:3});
+	let errors = req.validationErrors();
+
+	if(errors){
+		req.flash('error','Error is there');
+	}
+
+    var x=Math.random()*99999+10000;
+    nexmo.message.sendSms('Nexmo',mobileno,x);
+	res.render('project5records2',{number:mobileno});
+});
+
+app.post('/record3',function(req,res){
+    
+    Record.find({},function(err,data){
+    	if(err) throw err;
+    	res.render('project5records3',{recorddata:data});
+    });
+});
+
+
+app.get('/nexttest/:id',function(req,res){
+
+    var query={_id:req.params.id};
+	Test.find(query,function(err,data){
+		if(err) throw err;
+		app.locals.lab=data;
+		res.render('project5labpage1');
+	});
+});
+
+app.post('/testfinal',urlencodedParser,function(req,res){
+
+	req.checkBody('pname','Name should be atleast 3 characters long').isLength({min:3});
+	req.checkBody('pnumber','Phone Number should be valid').isMobilePhone();
+	req.checkBody('page','Age should be valid').isNumeric();
+	req.checkBody('pemail','Email should be valid').isEmail();
+	let errors = req.validationErrors();
+	if(errors){
+			req.flash('info',errors);
+			res.render('project5labpage1',{msgs:req.flash('info')});
+	}
+    else{
+	var p1=Patient({pname:req.body.pname,pnumber:req.body.pnumber,page:req.body.page,pemail:req.body.pemail,
+		pgender:req.body.pgender,pdate:req.body.pdate,ptime:req.body.ptime,paddress:req.body.paddress}).save(function(err){
+		if(err) throw err;
+	});
+	res.redirect('/testfinalconfirm');
+}
+});
+
+app.get('/testfinalconfirm',function(req,res){
+
+	Patient.find({},function(err,data){
+		if(err) throw err;
+		res.render('project5testconfirm',{confirm:data});
+	});
+});
+
+app.get('/delete/:id',function(req,res){
+    
+    var query={_id:req.params.id};
+	Record.remove(query,function(err){
+		if(err) throw err;
+	});
+	Record.find({},function(err,data){
+    	if(err) throw err;
+    	res.render('project5records3',{recorddata:data});
+    });
+
+});
+
+app.get('/tests',function(req,res){
+
+	Test.find({},function(err,data){
+		if(err) throw err;
+		res.render('project5tests',{data:data});
+	});
+});
+
+app.post('/testpage1',urlencodedParser,function(req,res){
+
+	var query={test:req.body.testname};
+	Test.find(query,function(err,data){
+		if(err) throw err;
+		res.render('project5test2',{testdetails:data});
+	})
+});
+
+app.get('/share/:id',function(req,res){
+    
+    res.render('project5recordshare');
+
+});
+
+app.post('/record4',urlencodedParser,function(req,res){
+ 
+	var record1=Record({title:req.body.title,date:req.body.date,type:req.body.type,name:req.body.fullname}).save(function(err){
+		if(err) throw err;
+	});
+
+	Record.find({},function(err,data){
+    	if(err) throw err;
+    	res.render('project5records3',{recorddata:data});
+    });
+ 
+});
+
+app.get('/upload',function(req,res){
+
+	res.render('project5uploadrecord');
 });
 
 app.post('/find',urlencodedParser,function(req,res){
@@ -132,32 +288,72 @@ app.get('/adddoctors',function(req,res){
 
 app.post('/adddoctors',urlencodedParser,function(req,res){
     
-    req.checkBody('name','name is required').notEmpty();
-	req.checkBody('states','state is required').notEmpty();
+    req.checkBody('name','Name should be atleast 3 characters long').isLength({min:3});
+	req.checkBody('phnnumber','number is required').isMobilePhone();
+	req.checkBody('state','State is required').isEmpty();
+	req.checkBody('category','Category is required').isEmpty();
+	req.checkBody('city','City is required').isEmpty();
     let errors = req.validationErrors();
 	if(errors){
 			res.render('project5errors',{errors:errors});
 		}
 		else
 		{    
-			var doctorThree= Doctor({phnnumber:req.body.phnnumber,name:req.body.name,state:req.body.states,category:req.body.category,accepting:req.body.accepting,pname:req.body.pname,address:req.body.address,year:req.body.year,city:req.body.city,zipcode:req.body.code}).save(function(err,data){
+			var doctorThree= Doctor({phnnumber:req.body.phnnumber,name:req.body.name,state:req.body.states,
+									category:req.body.category,accepting:req.body.accepting,pname:req.body.pname,
+									address:req.body.address,year:req.body.year,
+									city:req.body.city,zipcode:req.body.code}).save(function(err,data){
 			if(err) throw err;
 });
          res.redirect('/');
 };
 });
 
-app.get('/bookingform',function(req,res){
-
+app.get('/bookingform/:name',function(req,res){
+    
+    app.locals.docname=req.params.name;
 	res.render('project5bookingform');
 });
 
-app.post('bookingform',urlencodedParser,function(req,res){
+app.post('/bookingform/:docname',urlencodedParser,function(req,res){
+    
+    req.checkBody('name','Name should be atleast 3 characters long').isLength({min:3});
+	req.checkBody('mobile','Phone Number should be valid').isMobilePhone();
+	req.checkBody('age','Age should be valid').isNumberic();
+	req.checkBody('email','Email should be valid').isEmail();
+	let errors = req.validationErrors();
+	if(errors){
+			console.log(errors);
+	}
 
-	var appointmentOne=Appointment({name:req.body.name,age:req.body.age,date:req.body.date}).save(function(err,data){
+	var appointmentOne=Appointment({mobile:req.body.mobile,email:req.params.email,docname:req.params.docname,
+		name:req.body.name,age:req.body.age,date:req.body.date}).save(function(err,data){
 	if(err) throw err;
+	});
+	var transporter = nodemailer.createTransport({
+  	service: 'gmail',
+    auth: {
+        user: "jainanjali043@gmail.com",
+        pass: "anjali043"
+    }
 });
 
+var mailOptions = {
+  from: 'jainanjali043@gmail.com',
+  to: req.body.email,
+  subject: 'Appointment related mail',
+  text: 'Your appointment is fixed on your mentioned date..' 
+};
+
+transporter.sendMail(mailOptions, function(error, response){
+  if (error) {
+    console.log(error);
+  } 
+  else {
+    console.log('Email sent: ' + response.message);
+  }
+});
+res.redirect('/');
 });
 
 app.get('/addcategory',function(req,res){
@@ -167,6 +363,13 @@ app.get('/addcategory',function(req,res){
 
 app.post('/addcategory',urlencodedParser,function(req,res){
 
+	req.checkBody('category','Category is required').isEmpty();
+
+	let errors = req.validationErrors();
+	if(errors){
+			console.log(errors);
+	}
+
 	var categoryThree= Category({category:req.body.category}).save(function(err,data){
 	if(err) throw err;
 });
@@ -175,10 +378,52 @@ app.post('/addcategory',urlencodedParser,function(req,res){
 
 app.get('/details/:id',function(req,res){
 
-	Doctor.findById(req.params.id,function(err,data){
+	var query={_id:req.params.id};
+	Doctor.find(query,function(err,data){
 		if(err) throw err;
-		res.render('project5details',{doctor:data});
+		//console.log(data[0]);
+		app.locals.doctor = data[0];
+		res.render('project5details',{doctor:data[0]});
 });
 });
 
+// app.get('/filters',function(req,res){
+
+// 	res.render('project5filterpage');
+// });
+
+// app.post('/filtered',urlencodedParser,function(req,res){
+
+// 	var query={gender:req.body.gender,fee:req.body.fee};
+// 	Doctor.find(query,function(err,data){
+// 		if(err) throw err;
+// 		res.render('project5browse',{doctors:data});
+// 	});
+// });
+
+app.get('/share_exp/:id' , function(req,res){
+
+	Doctor.find({_id:req.params.id}, function(err , data){
+		if(err) throw err;
+		res.render('project5shareexp' , {doctor : data[0]});
+	});
+});
+
+app.post('/exp/:id' , urlencodedParser , function(req,res){
+
+	var {recommend,issue,waittime,quality,experience,name,email,number} = req.body;
+	var exp = {recommend,issue,waittime,quality,experience,name,email,number};
+	Doctor.update({_id:req.params.id}, {$push : {exp:exp}} , function(err){
+		if(err) throw err;
+	});
+	//??
+	res.redirect('/details/'  + req.params.id)
+});
+
+//app.get('/contactdoc/:id',function(req,res))
+
 app.listen('8080');
+
+//see front end once again
+// filter wla
+//chat wla
